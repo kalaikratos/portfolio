037ac0137452369de54e40ad1162a633825dbcec
Kalaiyarasan Dass <kalaiarasan2796@gmail.com>
2024-12-20 12:52:55 +0530

    update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiGit
+  DiGit,
+  DiBootstrap
 } from "react-icons/di";
 import {
   SiRedis,
@@ -21,9 +22,7 @@ import { TbBrandGolang } from "react-icons/tb";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
+     
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
@@ -44,7 +43,9 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
-     
+      <Col xs={4} md={2} className="tech-icons">
+        <DiBootstrap />
+      </Col>
      
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
