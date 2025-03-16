a608048e56c760a5279404e2673fced87e11ee71
Kalaiyarasan Dass <kalaiarasan2796@gmail.com>
2025-03-16 10:18:37 +0530

    new

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Kalaiyarasan Dass Resume.pdf";
+import pdf from "../../Assets/../Assets/Kalaiyarasan_Dass_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
